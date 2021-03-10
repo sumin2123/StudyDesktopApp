ffbcf8ce27b4e4feaffe22d39a5939cbd6803884
sumin2123 <mini247269@naver.com>
2021-03-11 01:25:18 +0900

    정리완료 커밋, 언제자누...

---
+++ WinformApp/PracticeWinApp/FlagWinAPp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlagWinAPp
+{
+    public partial class GreetingFrm : Form
+
+    {
+        private bool isHello = false; // flag상태를 저장.
+        public GreetingFrm()
+        {
+            InitializeComponent();
+            isHello = true; //아침임. 초기화
+        }
+
+        private void GreetingFrm_Load(object sender, EventArgs e)
+        {
+            LblGreeting.Text = string.Empty; 
+        }
+
+        private void BtnGreeting_Click(object sender, EventArgs e)
+        {
+            if(isHello == true)
+            {
+                LblGreeting.Text = "Good Morning!!";
+                isHello = false;
+                BtnGreeting.Text = "저녁인사";
+            }
+            else if (isHello == false)
+            {
+                LblGreeting.Text = "Good Bye~~~~~~!";
+                isHello = true;
+                BtnGreeting.Text = "아침인사";
+            }
+        }
+    }
+}
---
+++ WinformApp/PracticeWinApp/PracticeWinApp/Views/FrmChild.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace PracticeWinApp.Views
+{
+    partial class FrmChild
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnQ = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnQ
+            // 
+            this.BtnQ.Location = new System.Drawing.Point(107, 18);
+            this.BtnQ.Name = "BtnQ";
+            this.BtnQ.Size = new System.Drawing.Size(114, 46);
+            this.BtnQ.TabIndex = 0;
+            this.BtnQ.Text = "질문";
+            this.BtnQ.UseVisualStyleBackColor = true;
+            this.BtnQ.Click += new System.EventHandler(this.BtnQ_Click);
+            // 
+            // FrmChild
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 76);
+            this.Controls.Add(this.BtnQ);
+            this.Name = "FrmChild";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "자식폼";
+            this.Load += new System.EventHandler(this.FrmChild_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnQ;
+    }
+}
