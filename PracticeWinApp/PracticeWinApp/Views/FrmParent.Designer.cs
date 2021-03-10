4d39b892b9b7be04418dae8d8dc508ae8d29f72a
sumin2123 <mini247269@naver.com>
2021-03-11 01:11:14 +0900

    3/9 수업 정리 완료

---
+++ PracticeWinApp/PracticeWinApp/Views/FrmParent.Designer.cs
@@ -0,0 +1,51 @@
+﻿
+namespace PracticeWinApp.Views
+{
+    partial class FrmParent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmParent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 352);
+            this.MaximizeBox = false;
+            this.Name = "FrmParent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "부모폼";
+            this.Load += new System.EventHandler(this.FrmParent_Load);
+            this.ParentChanged += new System.EventHandler(this.FrmParent_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ PracticeWinApp/RadioWinApp/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace RadioWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RdbMale = new System.Windows.Forms.RadioButton();
+            this.RdbFemale = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RdbChina = new System.Windows.Forms.RadioButton();
+            this.RdbJapan = new System.Windows.Forms.RadioButton();
+            this.RdbOthers = new System.Windows.Forms.RadioButton();
+            this.RdbKorean = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.BtnResult = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RdbMale
+            // 
+            this.RdbMale.AutoSize = true;
+            this.RdbMale.Location = new System.Drawing.Point(33, 41);
+            this.RdbMale.Name = "RdbMale";
+            this.RdbMale.Size = new System.Drawing.Size(68, 23);
+            this.RdbMale.TabIndex = 4;
+            this.RdbMale.TabStop = true;
+            this.RdbMale.Text = "남자";
+            this.RdbMale.UseVisualStyleBackColor = true;
+            // 
+            // RdbFemale
+            // 
+            this.RdbFemale.AutoSize = true;
+            this.RdbFemale.Location = new System.Drawing.Point(143, 41);
+            this.RdbFemale.Name = "RdbFemale";
+            this.RdbFemale.Size = new System.Drawing.Size(68, 23);
+            this.RdbFemale.TabIndex = 8;
+            this.RdbFemale.TabStop = true;
+            this.RdbFemale.Text = "여자";
+            this.RdbFemale.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RdbChina);
+            this.groupBox1.Controls.Add(this.RdbJapan);
+            this.groupBox1.Controls.Add(this.RdbOthers);
+            this.groupBox1.Controls.Add(this.RdbKorean);
+            this.groupBox1.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.Location = new System.Drawing.Point(37, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(199, 234);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "국가";
+            // 
+            // RdbChina
+            // 
+            this.RdbChina.AutoSize = true;
+            this.RdbChina.Location = new System.Drawing.Point(33, 87);
+            this.RdbChina.Name = "RdbChina";
+            this.RdbChina.Size = new System.Drawing.Size(68, 23);
+            this.RdbChina.TabIndex = 7;
+            this.RdbChina.TabStop = true;
+            this.RdbChina.Text = "중국";
+            this.RdbChina.UseVisualStyleBackColor = true;
+            // 
+            // RdbJapan
+            // 
+            this.RdbJapan.AutoSize = true;
+            this.RdbJapan.Location = new System.Drawing.Point(33, 134);
+            this.RdbJapan.Name = "RdbJapan";
+            this.RdbJapan.Size = new System.Drawing.Size(68, 23);
+            this.RdbJapan.TabIndex = 6;
+            this.RdbJapan.TabStop = true;
+            this.RdbJapan.Text = "일본";
+            this.RdbJapan.UseVisualStyleBackColor = true;
+            // 
+            // RdbOthers
+            // 
+            this.RdbOthers.AutoSize = true;
+            this.RdbOthers.Location = new System.Drawing.Point(33, 181);
+            this.RdbOthers.Name = "RdbOthers";
+            this.RdbOthers.Size = new System.Drawing.Size(118, 23);
+            this.RdbOthers.TabIndex = 5;
+            this.RdbOthers.TabStop = true;
+            this.RdbOthers.Text = "그 외 국가";
+            this.RdbOthers.UseVisualStyleBackColor = true;
+            // 
+            // RdbKorean
+            // 
+            this.RdbKorean.AutoSize = true;
+            this.RdbKorean.Location = new System.Drawing.Point(33, 40);
+            this.RdbKorean.Name = "RdbKorean";
+            this.RdbKorean.Size = new System.Drawing.Size(106, 23);
+            this.RdbKorean.TabIndex = 4;
+            this.RdbKorean.TabStop = true;
+            this.RdbKorean.Text = "대한민국";
+            this.RdbKorean.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.RdbFemale);
+            this.groupBox2.Controls.Add(this.RdbMale);
+            this.groupBox2.Font = new System.Drawing.Font("굴림", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox2.Location = new System.Drawing.Point(254, 55);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(234, 97);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "성별";
+            // 
+            // BtnResult
+            // 
+            this.BtnResult.Location = new System.Drawing.Point(362, 236);
+            this.BtnResult.Name = "BtnResult";
+            this.BtnResult.Size = new System.Drawing.Size(126, 53);
+            this.BtnResult.TabIndex = 8;
+            this.BtnResult.Text = "결과";
+            this.BtnResult.UseVisualStyleBackColor = true;
+            this.BtnResult.Click += new System.EventHandler(this.BtnResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 341);
+            this.Controls.Add(this.BtnResult);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "나라 등록";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton RdbMale;
+        private System.Windows.Forms.RadioButton RdbFemale;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton RdbChina;
+        private System.Windows.Forms.RadioButton RdbJapan;
+        private System.Windows.Forms.RadioButton RdbOthers;
+        private System.Windows.Forms.RadioButton RdbKorean;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button BtnResult;
+    }
+}
+
